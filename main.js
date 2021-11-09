4eb2dcbd73907993638ad827fb95cd89500fbd64
James Burns <jrburns96@aol.com>
2021-11-09 12:41:20 -0500

    working liker

--- main.js
+++ main.js
@@ -3,9 +3,32 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let likes = document.getElementsByClassName("like-glyph");
+for (like of likes){
+  like.addEventListener("click", (e) => fakeserver(e));
+}
 
-
-
+function fakeserver(e){
+mimicServerCall()
+  .then(() => {
+    if(e.target.textContent === FULL_HEART){
+    e.target.classList.remove('activated-heart')
+    e.target.textContent = EMPTY_HEART;
+    }
+    else{
+    e.target.classList.add('activated-heart')
+    e.target.textContent = FULL_HEART;
+    console.log('server worked')}})  
+    .catch((error) => {
+    console.error('Random server error');
+  message = document.getElementById('modal');
+  message.classList.remove('hidden');
+  document.getElementById('modal-message').textContent = error;
+  console.log(document.getElementById('modal'));
+  setTimeout(() => message.classList.add('hidden'), 3000)
+  // let er = 
+  })
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
